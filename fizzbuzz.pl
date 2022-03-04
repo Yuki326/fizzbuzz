949fb3cdb722a8267e531809cde114e0ac2847a6
Yuki326 <s20a1035@gmail.com>
2022-03-04 23:39:04 +0900

    メッセージを配列で管理

--- fizzbuzz.pl
+++ fizzbuzz.pl
@@ -1,5 +1,8 @@
 my $check = 0;
 my @list = (3,5,7,11);
+my @msg = ("","Fizz","Buzz","FizzBuzz","セブン","","","","セブンイレブン",
+"","","","","","","いい気分");
+
 for(my $i=0;$i<2000;$i++){
   $check = 0b0000;#各数字の倍数かを管理
   foreach my $j(0 .. $#list){
@@ -7,13 +10,5 @@ for(my $i=0;$i<2000;$i++){
  		$check += 1 << $j;
     }
   }
-  if($check == 0b0001){print("Fizz\n")}
-  elsif($check == 0b0010){print("Buzz\n")}
-  elsif($check == 0b0011){print("FizzBuzz\n")}
-  elsif($check == 0b0100){print("セブン\n")}
-  elsif($check == 0b1000){print("イレブン\n")}
-  elsif($check == 0b1100){print("セブンイレブン\n")}
-  elsif($check == 0b1111){print("いい気分\n")}
-  else{print $i,"\n"}
-
+  print $msg[$check] ? $msg[$check]:$i,"\n"
 }
